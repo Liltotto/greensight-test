565003b5683e5cba41f175d5446bc43ccfd43349
Saadu <saadum603@gmail.com>
2024-05-04 10:54:54 +0300

    added directions border bottom

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -11,16 +11,8 @@ function Footer(props) {
 
             <div className="footer__right">
                 <span className="footer__right-item">322A, 2nd Floor, Zelenograd, Moscow, Russia</span>
-                <span className="footer__right-item">Directions</span>
+                <span className="footer__right-item footer__right-item_directions">Directions</span>
             </div>
-            {/* <div className="footer__top">
-                <span className="footer__top-item">+7 499 391-66-69</span>
-                <span className="footer__top-item">322A, 2nd Floor, Zelenograd, Moscow, Russia</span>
-            </div>
-            <div className="footer__bottom">
-                <span className="footer__bottom-item">mail@greensight.ru</span>
-                <span className="footer__bottom-item">Directions</span>
-            </div> */}
         </div>
     );
 }
